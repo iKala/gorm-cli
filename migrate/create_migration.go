e093acde6f27cb53e5bcd42786a2efd2f1d625a3
Calvin Huang <calvin.peak@capslock.tw>
2019-10-13 15:22:38 +0800

    feat(migration): add create_migration to create the migration template

---
+++ migrate/create_migration.go
@@ -0,0 +1,66 @@
+package migrate
+
+import (
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// CreateMigration - Create the migration file with template.
+func CreateMigration(purpose string) (string, error) {
+	if purpose == "" {
+		return "", errors.New("Missing purpose when creating migration")
+	}
+
+	// Create migration folder anyway.
+	_ = os.Mkdir("./migration", os.ModePerm)
+
+	migrationTemplate :=
+		`package main
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type migration string
+
+// Up - Changes for the migration.
+func (m migration) Up(db *gorm.DB) error {
+	return nil
+}
+
+// Down - Rollback changes for the migration.
+func (m migration) Down(db *gorm.DB) error {
+	return nil
+}
+
+var Migration migration`
+
+	targetFileName := MigrationTargetFolder + "/" + time.Now().Format("20060102150405") + "_" + purpose + ".go"
+
+	if fileExists(targetFileName) {
+		return "", errors.New("Migration exists")
+	}
+
+	err := ioutil.WriteFile(
+		targetFileName,
+		[]byte(migrationTemplate),
+		os.ModePerm,
+	)
+
+	if err != nil {
+		return "", errors.Wrap(err, "Create migration failed.")
+	}
+
+	return targetFileName, nil
+}
