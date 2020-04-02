275890b59c16205e5e7d4847dd69493f927462a8
Calvin Huang <calvin.peak@capslock.tw>
2020-04-03 03:57:19 +0800

    feat: support init db to create a connection plugin accords to gorm-cli.yaml settings

--- main.go
+++ main.go
@@ -7,19 +7,51 @@ import (
 	"strconv"
 
 	"github.com/iKala/gorm-cli/migrate"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
+	migrate.MigrationTargetFolder = "./migration"
+
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Something wrong when getting current path.")
+		return
+	}
+
 	if len(os.Args) == 1 {
-		fmt.Println("Option of migration is needed - (db:migrate / db:rollback / db:create_migration)")
+		fmt.Println("Option of migration is needed - (db:init db:migrate / db:rollback / db:create_migration)")
 		return
 	}
 
 	var migrateAction string
 	if migrateAction = os.Args[1]; migrateAction == "" {
-		fmt.Println("Empty option is not allowed - (db:migrate / db:rollback / db:create_migration)")
+		fmt.Println("Empty option is not allowed - (db:init db:migrate / db:rollback / db:create_migration)")
 		return
 	}
+	if migrateAction == "db:init" {
+		bytes, err := ioutil.ReadFile(path + "/.gorm-cli.yaml")
+		if err != nil {
+			fmt.Println(".gorm-cli.yaml not exists, you must create one for gorm-cli connecting to DB. https://github.com/iKala/gorm-cli/blob/master/README.md")
+			return
+		}
+
+		connection := migrate.Connection{}
+		if err := yaml.Unmarshal(bytes, &connection); err != nil {
+			fmt.Println("Failed to parse .gorm-cli.yaml, might be syntax error. https://github.com/iKala/gorm-cli/blob/master/.gorm-cli.yaml")
+			return
+		}
+
+		fileName, err := migrate.CreateConnection(connection)
+		if err != nil {
+			fmt.Println("Initiail connection file failed.")
+			return
+		}
+
+		fmt.Println("Connection file created", fileName)
+		return
+	}
+
 	rollbackStep := int64(-1)
 	if migrateAction == "db:rollback" && len(os.Args) == 3 {
 		argStep, err := strconv.ParseInt(os.Args[2], 10, 64)
@@ -40,15 +72,13 @@ func main() {
 		}
 	}
 
-	migrate.MigrationTargetFolder = "./migration"
-
 	files, err := ioutil.ReadDir(migrate.MigrationTargetFolder)
 	if err != nil {
 		fmt.Println("Migration folder not exists.")
 		return
 	}
 
-	db := model.InitDB()
+	db := migrate.NewDB()
 	defer db.Close()
 
 	db.AutoMigrate(&migrate.GormMeta{})
