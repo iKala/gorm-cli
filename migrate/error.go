ffad7627d631caac73b8ac9dd03bd15ae0c3b8be
Calvin Huang <calvin.peak@casplock.tw>
2022-11-20 16:19:23 +0800

    refactor: remove error pkg since it've be archived

--- migrate/error.go
+++ migrate/error.go
@@ -8,5 +8,4 @@ var (
 	ErrMigrationCanceled   = errors.New("rollback migration canceled")
 	ErrEmptyPurpose        = errors.New("missing purpose when creating migration")
 	ErrDuplicatedMigration = errors.New("migration exists")
-	ErrNoMigration         = errors.New("there is no migration")
 )
